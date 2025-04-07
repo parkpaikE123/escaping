33f2eb479eed1713fd2e29a98f3cbf3aa749e5f2
wjdals2285 <wjdals2285@naver.com>
2025-04-07 21:47:46 +0900

    [류정민] {escaping프로젝트} - 테마 추가 화면 구성

--- src/main/java/com/ryu/escaping/Admin/AdminController.java
+++ src/main/java/com/ryu/escaping/Admin/AdminController.java
@@ -1,23 +1,40 @@
-package com.ryu.escaping.Admin;
+package com.ryu.escaping.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/theme/admin")
+@RequestMapping("/admin")
 public class AdminController {
 
-	// 테마 관리
-	@GetMapping("/theme/list-view")
+	// 관리자 메인
+	@GetMapping("/theme/main-view")
 	public String manager() {
 		return "admin/main";
+	}
+	// 지점 추가
+	@GetMapping("/branch/create-view")
+	public String addBranch() {
+		return "admin/branch/create";
+	}
+	
+	// 테마 관리
+	@GetMapping("/theme/list-view")
+	public String adminTheme() {
+		return "admin/theme/list";
 	}
 	
 	// 테마 추가
 	@GetMapping("/theme/create-view")
 	public String createTheme() {
-		return "admin/create";
+		return "admin/theme/create";
+	}
+	
+	// 예약 관리
+	@GetMapping("/reservation-view")
+	public String adminReservation() {
+		return "admin/reservation/list";
 	}
 	
 }
